6ee7bd6995995d01906a8fb02ab656219f45c700
Abdulrahman-Ibnaof <abdulrahman.abnaof@gmail.com>
2026-05-13 13:39:05 +0300

    initial devops project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello DevOps from Kubernetes 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    service: "devops-full-project"
+  });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
