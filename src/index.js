4f3ccb4d41f1ab9b1f3aa7c0b1a95d0a07333304
Abdulrahman-Ibnaof <abdulrahman.abnaof@gmail.com>
2026-05-14 09:47:48 +0300

    modify index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps from Kubernetes 🚀");
+  res.send("Hello DevOps from Kubernetes V1🚀");
 });
 
 app.get("/health", (req, res) => {
